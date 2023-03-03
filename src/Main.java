09d1a7931364f90d21fc187500999290f4130a67
Pinaki Roy <pinaki.roy77759@gmail.com>
2023-03-03 23:29:36 +0530

    addition done to the local repo

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        int sub = b - a;
+        int mul = a * b;
+        int quo = a / b;
+        int rem = a % b;
+        System.out.println("the addition is " +sum);
+        System.out.println("the subtraction is " +sub);
+        System.out.println("the multiplication is " +mul);
+        System.out.println("the quotiont is " +quo);
+        System.out.println("the reminder is " +rem);
+    }
+}
