0ebe83840fb85a1e3cab18cf155eed714f580f65
Pinaki Roy <pinaki.roy77759@gmail.com>
2023-03-05 21:06:47 +0530

    Added if else condition

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,56 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int mul = a * b;
-        int quo = a / b;
-        int rem = a % b;
-        System.out.println("the multiplication is " +mul);
-        System.out.println("the quotiont is " +quo);
-        System.out.println("the reminder is " +rem);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double a = sc.nextDouble();
+        System.out.println("Enter the first number");
+        double b = sc.nextDouble();
+        System.out.println("Enter a choice \n 1) Addition \n 2) Substraction \n 3) multiplication \n 4) quotient \n 5) reminder");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            double add = a + b;
+            System.out.println("the addition is " + add);
+        }else if (choice == 2){
+            double sub = a - b;
+            System.out.println("the substraction is " +sub);
+        }else if (choice == 3){
+            double mul = a * b;
+            System.out.println("the multiplication is " +mul);
+        }else if (choice == 4){
+            double quo = a / b;
+            System.out.println("the quotiont is " +quo);
+        }else if (choice == 5){
+            double rem = a % b;
+            System.out.println("the reminder is " +rem);
+        }
+        else{
+            System.out.println("This input is invalid");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        double sub = a - b;
+//        double mul = a * b;
+//        double quo = a / b;
+//        double rem = a % b;
+//
+//        System.out.println("the substraction is " +sub);
+//        System.out.println("the multiplication is " +mul);
+//        System.out.println("the quotiont is " +quo);
+//        System.out.println("the reminder is " +rem);
+//    }
+//}
